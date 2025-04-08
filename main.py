75f78c56c04afaeb5518afa7545363977c0dfbc6
DevopsIntern2025 <babitharani.s@gmail.com>
2025-04-08 15:57:27 +0530

    Add docstrings to pass pylint

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+"""Main FastAPI app"""
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
+    """Returns a welcome message"""
     return {"message": "Hello World, from FastAPI"}
--- test_main.py
+++ test_main.py
@@ -1,9 +1,12 @@
+"""Test file for FastAPI app"""
+
 from fastapi.testclient import TestClient
 from main import app
 
 client = TestClient(app)
 
 def test_read_root():
+    """Test for GET /"""
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello, FastAPI!"}
